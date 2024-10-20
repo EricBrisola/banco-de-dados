2791c923988c90ed1d0e5710b5622bc39562239b
Eric Brisola <123534981+EricBrisola@users.noreply.github.com>
2024-10-20 18:10:58 -0300

    atividade 4
    adicionei os ultimos exercicios

--- banco_atv_4.sql
+++ banco_atv_4.sql
@@ -7,45 +7,38 @@ SELECT m.nome, m.cpf
 SELECT m.nome AS nome_medico, f.nome AS nome_funcionario, f.cidade AS cidade_comum
 	FROM medicos m
     JOIN funcionarios f 
-    ON 
-		m.cidade = f.cidade;
+    ON m.cidade = f.cidade;
     
 SELECT p.codp, p.nome, c.hora
 	FROM pacientes p
     JOIN consultas c
-    ON 
-		p.codp = c.codp
+    ON p.codp = c.codp
     WHERE c.hora > '14:00';
  
 SELECT a.nroa AS numero, a.andar AS andar, m.especialidade
 	FROM ambulatorio a
     JOIN medicos m 
-    ON 
-		a.nroa = m.nroa
+    ON a.nroa = m.nroa
     WHERE m.especialidade = 'ortopedia' AND m.ativo = 1;
 
 SELECT p.nome, p.cpf
 	FROM pacientes p
     JOIN consultas c
-    ON 
-		p.codp = c.codp
+    ON p.codp = c.codp
 	WHERE consulta_data BETWEEN  "2024-06-13" AND  "2024-06-16";
     
 SELECT m.nome, m.idade
 	FROM medicos m
     JOIN consultas c
-    ON 
-		m.codm = c.codm
+    ON m.codm = c.codm
     JOIN pacientes p
-    ON 
-		p.codp = c.codp
+    ON p.codp = c.codp
     WHERE p.nome = 'Ana';
     
 SELECT m.codm, m.nome
 	FROM medicos m
     JOIN ambulatorio a
-    ON 
-		m.nroa = a.nroa
+    ON m.nroa = a.nroa
     WHERE nome != 'Pedro' 
     AND m.nroa = (
 		SELECT nroa 
@@ -55,11 +48,9 @@ SELECT m.codm, m.nome
 SELECT p.nome AS paciente, p.cpf
 	FROM pacientes p 
     JOIN consultas c
-    ON 
-		p.codp = c.codp
+    ON p.codp = c.codp
     JOIN medicos m
-    ON 
-		c.codm = m.codm
+    ON c.codm = m.codm
     WHERE m.especialidade = 'ortopedia' AND c.consulta_data < '2024-06-21';
     
 SELECT f.nome, f.salario
@@ -97,21 +88,57 @@ SELECT c.consulta_data, c.hora, m.nome AS medico, p.nome AS paciente, a.nroa
 	LEFT JOIN pacientes p ON c.codp = p.codp
 	LEFT JOIN ambulatorio a ON a.nroa = m.nroa;
 */
-    
-/*1.
-Realize as seguintes consultas usando subconsultas com IN,
-ANY, ALL e/ou EXISTS, ou subconsultas na cláusula FROM:
 
-a. Buscar nome e CPF dos médicos que são pacientes do hospital
-b. Buscar código e nome dos pacientes com consulta marcada
-para horários após às 14 horas.
-c. Buscar o número e andar dos ambulatórios onde nenhum
-médico dá atendimento.
-d. Buscar o número e o andar de todos os ambulatórios, exceto o
-de menor capacidade.
-e. Buscar nome e CPF dos médicos que atendem em
-ambulatórios com capacidade superior à menor capacidade dos
-ambulatórios do primeiro andar (usando somente estas informações).
-f. DESAFIO: buscar nome e CPF dos médicos que têm/tiveram
-consultas com todos os pacientes menores de 25 anos.
- */
+SELECT m.nome, m.cpf
+	FROM medicos m
+    WHERE m.cpf IN (
+		SELECT p.cpf
+		FROM pacientes p
+        );
+        
+SELECT p.codp, p.nome
+	FROM pacientes p
+    WHERE p.codp IN (
+		SELECT c.codp
+        FROM consultas c
+        WHERE hora > '14:00'
+        );
+
+SELECT a.nroa, a.andar
+	FROM ambulatorio a
+    WHERE NOT EXISTS (
+		SELECT m.nroa
+        FROM medicos m
+        WHERE m.nroa = a.nroa
+    );
+    
+SELECT a.nroa, a.andar, a.capacidade
+	FROM ambulatorio a
+    WHERE a.capacidade > (
+		SELECT MIN(a.capacidade)
+        FROM ambulatorio a
+    );
+
+SELECT m.nome, m.cpf
+	FROM medicos m
+	JOIN ambulatorio a
+    ON m.nroa = a.nroa
+    WHERE a.capacidade > (
+		SELECT MIN(a.capacidade)
+		FROM ambulatorio a
+		WHERE a.andar = 1
+    );
+    
+SELECT m.nome AS doutor, m.cpf, COUNT(DISTINCT p.codp) AS pacientes_menores_de_25_atendidos
+	FROM medicos m
+    JOIN consultas c
+    ON m.codm = c.codm
+    JOIN pacientes p
+    ON c.codp = p.codp
+    WHERE p.idade < 25
+    GROUP BY m.nome, m.cpf
+	HAVING COUNT(p.codp) >= (
+		SELECT COUNT(DISTINCT p.codp)
+		FROM pacientes p
+		WHERE p.idade < 25
+		);
